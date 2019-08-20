da4de47f1c7a4abbedc0b7f51b3fd791cb03868d
Luca Falasco <falasco.luca@gmail.com>
2019-08-20 15:50:37 +0200

    css fix for ResponsiveWrapper

--- packages/datalith-util/src/ResponsiveWrapper.tsx
+++ packages/datalith-util/src/ResponsiveWrapper.tsx
@@ -12,15 +12,17 @@ export default function ResponsiveWrapper<P extends CommonProps>(Component: Comp
       ) : (
         <SizeMe monitorWidth monitorHeight>
           {({ size }) => (
-            <div style={{ width: '100%', height: '100%' }}>
-              <Component
-                {...this.props}
-                style={{ ...size, ...this.props.style }}
-                size={{
-                  width: (this.props.style && this.props.style.width) || size.width,
-                  height: (this.props.style && this.props.style.height) || size.height,
-                }}
-              />
+            <div style={{ position: 'relative', height: '100%' }}>
+              <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+                <Component
+                  {...this.props}
+                  style={{ ...size, ...this.props.style }}
+                  size={{
+                    width: (this.props.style && this.props.style.width) || size.width,
+                    height: (this.props.style && this.props.style.height) || size.height,
+                  }}
+                />
+              </div>
             </div>
           )}
         </SizeMe>
