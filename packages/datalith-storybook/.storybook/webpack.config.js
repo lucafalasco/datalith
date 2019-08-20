e4795acc02b086b7f5ce0d010bab3d137aadb3ff
Luca Falasco <falasco.luca@gmail.com>
2019-08-20 16:05:51 +0200

    upgrade storybook, cleanup config files

--- packages/datalith-storybook/.storybook/webpack.config.js
+++ packages/datalith-storybook/.storybook/webpack.config.js
@@ -1,31 +1,29 @@
 const path = require('path')
 
 module.exports = ({ config, mode }) => {
-  config.module.rules.push(
-    {
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-        {
-          loader: require.resolve('@storybook/addon-storysource/loader'),
-          options: {
-            parser: 'typescript',
-            prettierConfig: {
-              printWidth: 100,
-              semi: false,
-              singleQuote: true,
-              trailingComma: 'all',
-            },
-          },
-        },
-      ],
-    },
-  )
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('ts-loader'),
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
+      // {
+      //   loader: require.resolve('@storybook/addon-storysource/loader'),
+      //   options: {
+      //     parser: 'typescript',
+      //     prettierConfig: {
+      //       printWidth: 100,
+      //       semi: false,
+      //       singleQuote: true,
+      //       trailingComma: 'all',
+      //     },
+      //   },
+      // },
+    ],
+  })
   config.resolve.extensions.push('.ts', '.tsx')
   return config
 }
